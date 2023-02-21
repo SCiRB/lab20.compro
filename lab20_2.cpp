c5aa9cfa33ed4b4bb3ae302c666f42fd7a9debcd
Lica <Tanapat_choeichomsri@cmu.ac.th>
2023-02-21 17:04:11 +0700

    completed 20.1 20.2

--- lab20_2.cpp
+++ lab20_2.cpp
@@ -1,12 +1,34 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *R1, Rect *R2){
+    double r1right = R1->x + R1->w;
+    double r1bot = R1->y - R1->h;
+    double r2right = R2->x + R2->w;
+    double r2bot = R2->y - R2->h;
+    double right,left,bot,top;
+    
+    if(r1right > r2right){right = r2right;}
+    else {right = r1right;}
+    if(R1->x > R2->x){left = R1->x;}
+    else {left = R2->x;}
+    
+    if(R1->y > R2->y){top = R2->y;}
+    else {top = R1->y;}
+    if(r1bot > r2bot){bot = r1bot;}
+    else {bot = r2bot;}
 
+    double w = abs(right-left);
+    double h = abs(top-bot);
+    if(right < left || top < bot){
+		w = 0;
+		h = 0;
+	}
 
-
+    return w*h;
 }
